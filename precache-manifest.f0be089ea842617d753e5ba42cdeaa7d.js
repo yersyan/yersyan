a4c4b646b2c93049daed243d77d0c91395bb604a
YerSar87 <esargsyan1987@gmail.com>
2020-06-13 17:30:11 +0400

    Updates

---
+++ precache-manifest.f0be089ea842617d753e5ba42cdeaa7d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606b8b6d968369b5ba53d5c5ce711c71",
+    "url": "/yersyan/index.html"
+  },
+  {
+    "revision": "bf6a488523e262813356",
+    "url": "/yersyan/static/css/main.f8413478.chunk.css"
+  },
+  {
+    "revision": "f60e3a1285ba7ad13e45",
+    "url": "/yersyan/static/js/2.4869b403.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/yersyan/static/js/2.4869b403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6a488523e262813356",
+    "url": "/yersyan/static/js/main.18040858.chunk.js"
+  },
+  {
+    "revision": "9560761892d5dd73357b",
+    "url": "/yersyan/static/js/runtime-main.ef6a29f7.js"
+  },
+  {
+    "revision": "87d5873e077d27e23f4e1621a0010341",
+    "url": "/yersyan/static/media/css.87d5873e.png"
+  },
+  {
+    "revision": "0b26829bc5dd442d189a8cc299c02ae7",
+    "url": "/yersyan/static/media/en.0b26829b.png"
+  },
+  {
+    "revision": "662edfd249f15aa664800043b8d7a30e",
+    "url": "/yersyan/static/media/git.662edfd2.png"
+  },
+  {
+    "revision": "b4002e70b6cb73b1093d83e2b8e6c733",
+    "url": "/yersyan/static/media/int.b4002e70.png"
+  },
+  {
+    "revision": "c7e23deed07fd2f09c71cc083a3a1330",
+    "url": "/yersyan/static/media/js.c7e23dee.png"
+  },
+  {
+    "revision": "a584bd8b929e4c161ace22ec03c6a844",
+    "url": "/yersyan/static/media/php.a584bd8b.png"
+  },
+  {
+    "revision": "f2fb5371ce95f01aa18bd7962681b2aa",
+    "url": "/yersyan/static/media/ps.f2fb5371.png"
+  },
+  {
+    "revision": "13368578740b7cfc459e54004312bc14",
+    "url": "/yersyan/static/media/react.13368578.png"
+  }
+]);
